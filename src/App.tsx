6529a56b04cbc91ca8bb95f195d9299b83007028
Nora Liu <noraliu0830@gmail.com>
2021-09-09 00:37:36 +0800

    feat: add header and tab ui component

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import ProposalDetail from "./components/ProposalDetail";
 import ProposalList from "./components/ProposalList";
-
-import { Main, Layout } from "@aragon/ui";
+// @ts-ignore
+import { Main, Layout, Header, Tabs } from "@aragon/ui";
 
 function App() {
+  const [selected, setSelected] = useState(0);
+
   return (
     <Router>
       <Main>
+        <Header primary={"Aave Governance Dashboard"} />
+
+        <Tabs
+          items={["On-Chain", "Off-Chain"]}
+          selected={selected}
+          onChange={setSelected}
+        />
         <Switch>
           <Route path="/proposal/:ipfsHash">
             <Layout>
