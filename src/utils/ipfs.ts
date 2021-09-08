970b00beb68a0374a6f499c15a5c6be44cede5e2
Nora Liu <noraliu0830@gmail.com>
2021-09-09 01:36:50 +0800

    feat: add parse_ipfs_hash

--- src/utils/ipfs.ts
+++ src/utils/ipfs.ts
@@ -10,3 +10,9 @@ export async function get(cid: string) {
   const getFile = await ipfs.object.get(cid);
   return Buffer.from(getFile.Data).toString();
 }
+
+export async function parseIpfsHash(ipfsHash: string) {
+  const format = "12" + "20" + ipfsHash.substring(2);
+  const cid = require("bs58").encode(Buffer.from(format, "hex"));
+  return cid;
+}
