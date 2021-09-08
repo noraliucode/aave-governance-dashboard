a6fd9db064e36f580ad3b717c919ac63e78b07bc
Nora Liu <noraliu0830@gmail.com>
2021-09-09 01:25:08 +0800

    feat: add proposal types

---
+++ src/types.ts/proposal.ts
@@ -0,0 +1,30 @@
+export enum selectedType {
+  offChain = 1,
+  onChain,
+}
+
+export type ProposalPropType = {
+  selected: number;
+};
+
+export type ProposalType = {
+  title: string;
+  againstVotes: number;
+  forVotes: string;
+  proposalState: number;
+  basename: string;
+  status: string;
+};
+
+export enum ProposalState {
+  Pending = 0,
+  Canceled,
+  Active,
+  Failed,
+  Succeeded,
+  Queued,
+  Expired,
+  Executed,
+}
+
+export {};
