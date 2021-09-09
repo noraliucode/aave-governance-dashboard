1b04a36ebb96506b96e6903ec43d1b9886b5e37a
Nora Liu <noraliu0830@gmail.com>
2021-09-09 16:25:22 +0800

    feat: add data view on proposal list

--- src/App.tsx
+++ src/App.tsx
@@ -27,7 +27,7 @@ function App() {
           </Route>
           <Route path="/">
             <Layout>
-              <ProposalList />
+              <ProposalList selected={selected} />
             </Layout>
           </Route>
         </Switch>
--- src/components/ProposalList.tsx
+++ src/components/ProposalList.tsx
@@ -1,8 +1,20 @@
 import React, { useCallback } from "react";
 import { useHistory } from "react-router-dom";
+// @ts-ignore
+import { DataView, IdentityBadge } from "@aragon/ui";
+import {
+  ProposalPropType,
+  ProposalType,
+  selectedType,
+  ProposalState,
+} from "../types.ts";
+import { useOnChain } from "../hooks";
 
-export default function ProposalList() {
+export default function ProposalList(props: ProposalPropType) {
+  const { selected } = props;
   const history = useHistory();
+  const onChain = useOnChain();
+
   const goToDetail = useCallback(
     (ipfsHash: string) => {
       history.push(`/proposal/${ipfsHash}`);
@@ -10,6 +22,28 @@ export default function ProposalList() {
     [history]
   );
 
-  let ipfsHash = "0x";
-  return <button onClick={() => goToDetail(ipfsHash)}>ProposalList</button>;
+  return (
+    <DataView
+      fields={["Title", "currentYesVote", "status", "ProposalState"]}
+      entries={
+        selected === selectedType.offChain
+          ? [{ title: 1, forVotes: 1 }]
+          : onChain
+      }
+      renderEntry={({
+        title,
+        againstVotes,
+        forVotes,
+        proposalState,
+        status,
+      }: ProposalType) => {
+        return [
+          <IdentityBadge entity={title} />,
+          <div>{forVotes}</div>,
+          <div>{status}</div>,
+          <div>{ProposalState[proposalState]}</div>,
+        ];
+      }}
+    />
+  );
 }
