7c72dbce7ab57404d038692f61fd702c992c3445
Nora Liu <noraliu0830@gmail.com>
2021-09-12 22:31:39 +0800

    feat: modify bar style

--- src/components/ProposalDetail.tsx
+++ src/components/ProposalDetail.tsx
@@ -1,23 +1,23 @@
 import React from "react";
 import { useParams, useHistory } from "react-router-dom";
 // @ts-ignore
-import { BackButton, Header } from "@aragon/ui";
+import { BackButton, Header, Bar } from "@aragon/ui";
 import { useProposal } from "../hooks";
 // @ts-ignore
 import marked from "marked";
 
 export default function ProposalDetail() {
   const { id } = useParams() as any;
-  console.log("id!!", id);
   const history = useHistory();
   const proposal = useProposal(id);
   const { title, description } = proposal;
-  console.log("proposal", proposal);
-  console.log("description", description);
 
   return (
     <div>
-      <BackButton onClick={() => history.push(`/`)} />
+      <Bar
+        primary={<BackButton label="Aave" onClick={() => history.push(`/`)} />}
+      />
+
       <Header primary={title} />
       <div dangerouslySetInnerHTML={{ __html: marked(description || "") }} />
     </div>
